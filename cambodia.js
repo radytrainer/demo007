9d0b810d18d41b38628d443f98e7f8c2e3a3037c
rady y <rady.y@passerellesnumeriques.org>
2019-08-14 13:29:26 +0700

    Update cambodia.js

--- cambodia.js
+++ cambodia.js
@@ -15,3 +15,49 @@ fetch(urls)
                 '</tr>';
         }
     });
+
+// var urls = "https://jsonplaceholder.typicode.com/comments";
+// var table = document.querySelector('#post');
+// fetch(urls)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         data.forEach(function (element) {
+//             if (element.id <= 5) {
+//                 table.innerHTML += `
+//                 <div class="card mt-5">
+//                    <div class="card-header">${element.name}</div>
+//                    <div class="card-body">${element.body}</div>
+//                    <div class="card-footer">${element.email} </div>
+//                </div>
+//             `;
+//             }
+
+//         })
+//     })
+//     .catch(function (error) {
+//         return console.log("Cannot get data");
+//     })
+
+// var url = "https://jsonplaceholder.typicode.com/comments";
+// var table = document.querySelector('#post');
+// var xmlRequest = new XMLHttpRequest();
+// xmlRequest.open('GET', url, true);
+// xmlRequest.onload = () => {
+//     data = JSON.parse(xmlRequest.responseText)
+//     data.forEach(function (element) {
+//         if (element.id <= 5) {
+//             table.innerHTML += `
+//                         <div class="card mt-5">
+//                            <div class="card-header">${element.name}</div>
+//                            <div class="card-body">${element.body}</div>
+//                            <div class="card-footer">${element.email} </div>
+//                        </div>
+//                     `;
+//         }
+
+//     })
+// }
+// xmlRequest.send();
+
